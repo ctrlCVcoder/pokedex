0aa340c66c2480d89e0b75f0b60af951cec17005
ctrlCVcoder <gero.spd@gmail.com>
2025-12-21 19:29:00 +0200

    add 3-1 Pokedex

--- main.go
+++ main.go
@@ -68,6 +68,11 @@ func main() {
 			description: "Inspect a pokemon",
 			callback:    commandInspect,
 		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "See your pokedex",
+			callback:    commandPokedex,
+		},
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -224,5 +229,13 @@ func commandInspect(cfg *config, args ...string) error {
 		fmt.Println("  -", typeInfo.Type.Name)
 	}
 
+	return nil
+}
+
+func commandPokedex(cfg *config, args ...string) error {
+	fmt.Println("Your Pokedex:")
+	for _, pokemon := range cfg.pokedex {
+		fmt.Printf(" - %s\n", pokemon.Name)
+	}
 	return nil
 }
